044950b82ea78e6e65433a679688a874a35fc2df
Kapil Tiwari <tiwarikapil216@gmail.com>
2025-02-21 22:16:55 +0530

    [Kapil Tiwari] Added uc1

---
+++ UC1_SayHello/src/main/java/com/example/UC1_SayHello/Uc1SayHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.UC1_SayHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc1SayHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc1SayHelloApplication.class, args);
+	}
+
+}
---
+++ UC1_SayHello/src/test/java/com/example/UC1_SayHello/Uc1SayHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC1_SayHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc1SayHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
